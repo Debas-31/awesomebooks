a56cb0f48b411748a7307f69af7e65cf0883de5f
RadiactiveJesus <rjllinasc@gmail.com>
2021-11-22 08:19:19 -0500

    Creating functions for adding, removing and displaying books

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-let bookCollection = [];
+let tempBooks = [];
+
+const addBook = () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const book = { id: new Date().getTime(), title, author };
+  if (localStorage.getItem('booksData') !== null) {
+    tempBooks = JSON.parse(localStorage.getItem('booksData'));
+    tempBooks.push(book);
+    const convertedBooks = JSON.stringify(tempBooks);
+    localStorage.setItem('booksData', convertedBooks);
+    window.location.reload();
+  } else {
+    tempBooks.push(book);
+    const convertedBooks = JSON.stringify(tempBooks);
+    localStorage.setItem('booksData', convertedBooks);
+    window.location.reload();
+  }
+};
+
+function removeBook(id) {
+    const booksData = localStorage.getItem('booksData');
+    const convertedBooks = JSON.parse(booksData);
+    const remainingBooks = convertedBooks.filter((book) => book.id !== id);
+    const removedBooks = JSON.stringify(remainingBooks);
+    localStorage.setItem('booksData', removedBooks);
+    window.location.reload();
+}
+
+const displayBooks = () => {
+    const booksData = localStorage.getItem('booksData');
+    const convertedBooks = JSON.parse(booksData);
+    document.getElementById('bookstatus').innerHTML = 'No books added';
+    if (convertedBooks && convertedBooks.length === 0) {
+      document.getElementById('bookstatus').innerHTML = 'No books added';
+    } else {
+      document.getElementById('bookstatus').innerHTML = '';
+      let html = '';
+      convertedBooks.forEach((book) => {
+        html += `<article>
+       <h2>${book.title}</h2>
+       <h2>${book.author}</h2>
+       <button data-book-id = "${book.id}" id = "remove-button" onclick="removeBook(${book.id})">Remove</button>
+     </article></br><hr>`;
+      });
+      document.getElementById('bookslist').innerHTML = html;
+    }
+};
+
+window.addEventListener('load', displayBooks);
+
+document.getElementById('addbook').addEventListener('click', addBook);
